b423e31b7f1e88b388294e02075a392bd62da4b0
kjradem <koenrademaker@outlook.com>
2019-03-31 14:42:22 +0200

    added rsID filter, reference SNP comparison and DEPICT formatting

--- GWAS/sum_stats_qc/sum_stats_qc.sh
+++ GWAS/sum_stats_qc/sum_stats_qc.sh
@@ -6,7 +6,7 @@
 # DATE: 31 March 2019
 
 
-# (0) Decare input files and data columns
+# DECLARE INPUT FILES AND DATA COLUMNS
 sum_stats=#/path/to/sum_stats
 imputed_snps=#/path/to/imputed_snps
 reference_snps=#/path/to/reference_snps
@@ -17,9 +17,12 @@ info_snp_col=# Set to SNP column
 p_col=# Set to P column
 a1_col=# Set to A1 column
 a2_col=# Set to A2 column
+snp_col=# Set to SNP (rsID) column
+chr_col=# Set to chromosome column
+pos_col=# Set to (base-pair) position column
 
 
-# (1) Perform QC (optional: write to tmp files)
+# PERFORM QC STEPS (OPTIONAL: WRITE TO TEMPORARY FILES)
 awk -v col="$maf_col" '$col<0.01 && $col>0.99' ${sum_stats} # Filter MAF (default: >0.01)
 
 awk -v col="$info_col" '$col>0.9' ${sum_stats} # Filter INFO score (default: >0.9)
@@ -31,3 +34,13 @@ awk 'NR == FNR {c[$1]++; next}; c[$1] > 0' ${imputed_snps} ${sum_stats}
 awk -v col="$p_col" '$col<0.05' ${sum_stats} # Filter P-value (default: <0.05)
 
 awk -v a1="$a1_col" -v a2="$a2_col" '($a1=="G" || $a1=="A" || $a1=="C" || $a1=="T") && ($a2=="G" || $a2=="A" || $a2=="C" || $a2=="T")' ${sum_stats} # Filter bi-allelic alleles
+
+grep 'rs' ${sum_stats} # Select SNPs with rsID
+
+awk -v n=1 '{s = gensub("^(([^:]*:){"n"}).*$", "\\1", 1); sub(".$","",s); print s}' ${sum_stats} # Extract rsID from full SNP name
+
+awk 'OFS=""; FNR>2 { for (i=1; i<=NF; i++) print "rs",$i }' ${reference_snps} > tmp_reference_snps # Filter reference SNPs (default: filtered MAGMA SNP synonyms, see README)
+mv tmp_reference_snps ${reference_snps}
+awk -v col="$snp_col" 'NR == FNR{c[$1]++;next};c[$col] > 0' ${reference_snps} ${sum_stats}
+
+awk -v snp="$snp_col" -v chr="$chr_col" -v pos="$pos_col" -v p="$p_col" 'BEGIN {OFS="\t"; print "SNP\tChr\tPos\tP"} FNR>1 {print $snp,$chr,$pos,$p}' ${sum_stats} # Convert to DEPICT input format
