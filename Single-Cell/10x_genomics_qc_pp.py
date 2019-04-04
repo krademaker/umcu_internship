3c9532849fe844344fde1f63fd70300f7cc53c96
kjradem <koenrademaker@outlook.com>
2019-04-04 14:41:33 +0200

    split pre-processing steps

--- Single-Cell/10x_genomics_qc_pp.py
+++ Single-Cell/10x_genomics_qc_pp.py
@@ -31,11 +31,14 @@ sc_data.obs['n_counts'] = sc_data.X.sum(axis=1).A1
 
 
 # Pre-process data
-sc.pp.filter_cells(sc_data, min_genes=200, min_counts=1) # Retain cells with at least 200 genes and 1 read count
+sc.pp.filter_cells(sc_data, min_counts=1) # Retain cells with at least 1 UMI count
+sc.pp.filter_cells(sc_data, min_genes=200) # Retain cells with at least 200 genes
 sc.pp.filter_genes(sc_data, min_cells=1) # Retain genes occurring in at least 1 cell
 sc_data=sc_data[sc_data.obs['percent_mito'] < 0.05,:] # Retain cells with fewer than 5% mitochondrial genes
 sc_data=sc_data[sc_data.obs['n_genes'] < 4000,:] # Retain cells with fewer than 4000 genes
 
 sc.pp.normalize_per_cell(sc_data, counts_per_cell_after=1e4, key_n_counts='original_counts') # Normalize to 10,000 counts per cell
+
+sc.pp.log1p(sc_data) # Log transform the data
 
 sc_data.to_df().to_csv(NORMALIZED_FILE) # Export pre-processed, normalized gene expression matrix
