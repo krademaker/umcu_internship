f034979646b5bd301587f834a0cb7473f76c12fa
kjradem <koenrademaker@outlook.com>
2019-04-08 15:56:23 +0200

    updated pre-processing steps

--- Single-Cell/10x_genomics_qc_restructure.py
+++ Single-Cell/10x_genomics_qc_restructure.py
@@ -22,11 +22,16 @@ sys.path.append("{}/Single-Cell/perslab-sc-library".format(path))
 from dropseq import get_average_by_celltype, standardize
 
 
+# Set loggings settings
+sc.settings.verbosity = 3
+sc.logging.print_versions()
+
+
 # Set variables
 filename_input_h5='1M_neurons_neuron20k.h5'
 filename_cluster_ids='20k_clusters.csv'
 genome='mm10'
-filename_out='1M_neurons_neuron_20k_normalized.csv'
+filename_out='1M_neurons_neuron_20k_qc_avg_std_celltype.csv'
 
 
 # Load data
@@ -35,20 +40,21 @@ sc_data.var_names_make_unique()
 sc_data.obs['cell_labels'] = pd.read_csv(filename_cluster_ids, header=None, dtype='category')[1].values
 
 
-# Determine cell quality metrics (percentage mitochondrial genes, n.o. counts)
+# Determine cell quality metrics (percentage mitochondrial genes, n.o. counts, n.o. genes)
 mito_genes=sc_data.var_names.str.startswith('mt-')
 sc_data.obs['percent_mito'] = np.sum(sc_data[:, mito_genes].X, axis=1).A1 / np.sum(sc_data.X, axis=1).A1
 sc_data.obs['n_counts'] = sc_data.X.sum(axis=1).A1
+sc.pp.filter_cells(sc_data, min_counts=0)
 
 
 # Pre-process data
-sc.pp.filter_cells(sc_data, min_counts=1) # Retain cells with at least 1 UMI count
-sc.pp.filter_cells(sc_data, min_genes=500) # Retain cells with at least 500 genes
-sc.pp.filter_genes(sc_data, min_cells=1) # Retain genes occurring in at least 1 cell
-sc.pp.filter_genes(sc_data, min_counts=1) # Retain genes with at least 1 UMI count
-sc_data=sc_data[sc_data.obs['percent_mito'] < 0.05,:] # Retain cells with fewer than 5% mitochondrial genes
-sc_data=sc_data[sc_data.obs['n_genes'] < 4000,:] # Retain cells with fewer than 4000 genes
 sc.pp.normalize_per_cell(sc_data, counts_per_cell_after=1e4, key_n_counts='original_counts') # Normalize to 10,000 counts per cell
+sc.pp.filter_cells(sc_data, min_counts=1) # Retain cells with >1 UMI counts
+sc.pp.filter_cells(sc_data, min_genes=500) # Retain cells with >500 genes
+sc.pp.filter_cells(sc_data, max_genes=4000) # Retain cells with <4000 genes
+sc.pp.filter_genes(sc_data, min_cells=1) # Retain genes occuring in >1 cell
+sc.pp.filter_genes(sc_data, min_counts=1) # Retain genes with >1 UMI counts
+sc_data=sc_data[sc_data.obs['percent_mito'] < 0.1,:] # Retain cells with fewer than 10% mitochondrial genes
 sc.pp.log1p(sc_data) # Log transform the data
 
 
@@ -58,3 +64,4 @@ cell_cluster_ids=pd.read_csv(filename_cluster_ids, sep=',', header=None, index_c
 cell_cluster_ids.columns=['cluster_id'] # Set DataFrame column name
 sc_data_avg_by_celltype=get_average_by_celltype(sc_data_df, cell_cluster_ids) # Average gene expression across cell types
 sc_data_standardized_by_celltype=standardize(sc_data_avg_by_celltype) # Standardize gene expression across cell types
+sc_data_standardized_by_celltype.to_csv(filename_out, skip_blank_lines=True, sep='\t') # Export file
