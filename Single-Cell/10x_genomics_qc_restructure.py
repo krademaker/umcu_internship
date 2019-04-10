029e434b5f3aecc3cf710ffb185c30304402d3c8
kjradem <koenrademaker@outlook.com>
2019-04-10 16:41:15 +0200

    finalized to do QC and restructure data

--- Single-Cell/10x_genomics_qc_restructure.py
+++ Single-Cell/10x_genomics_qc_restructure.py
@@ -1,67 +1,97 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python
 
-
-# ABOUT: Script for QC / pre-processing of 10x Genomics mouse brain scRNA-seq data
-# REQUIRED: 10x Genomics 1.3 million cells mouse brain dataset (20K cells subsample)
+# ABOUT: Script for QC and restructuring of 10x Genomics mouse brain scRNA-seq data
+# REQUIRED:     - HFD5 gene / cell matrix (see README)
+#               - Clustering output file (see README)
+#               - ENSEMBL to MGI mapping file (see README)
+#               - ENSEMBL mouse to human mapping file (see README)
 # AUTHOR: Koen Rademaker
-# DATE: 8 April 2019
+# DATE: 10 April 2019
 
 
-# Import packages
+# Step 1 - Import packages
 import numpy as np
 import pandas as pd
 import scanpy as sc
+import matplotlib
+matplotlib.use('Agg')
 import os
 import sys
 
 
-# Import "perslab-sc-library" repository
+# Step 2 - Import "perslab-sc-library" repository
 path_to_script = os.path.realpath(__file__).split('/')
 path = "/".join(path_to_script[0:(len(path_to_script)-2)])
 sys.path.append("{}/Single-Cell/perslab-sc-library".format(path))
-from dropseq import get_average_by_celltype, standardize
+from dropseq import get_average_by_celltype, standardize, normalize
+from gene_mapping import to_ensembl
 
 
-# Set loggings settings
-sc.settings.verbosity = 3
+# Step 3 - Set logging and image settings
 sc.logging.print_versions()
+sc.settings.verbosity = 2
+sc.settings.autosave = True
+sc.settings.set_figure_params(dpi=300)
 
 
-# Set variables
+# Step 4 - Set variables
 filename_input_h5='1M_neurons_neuron20k.h5'
 filename_cluster_ids='20k_clusters.csv'
 genome='mm10'
-filename_out='1M_neurons_neuron_20k_qc_avg_std_celltype.csv'
+filename_h5ad_out='1M_neurons_neuron_20k_qc.h5ad'
+filename_restructured_out='1M_neurons_neuron_20k_qc_avg_std_celltype.csv'
+filename_mapped_out='tmp_mapped_to_human.csv'
+filename_mouse_mapping='ensembl_v83_ensembl_mgisymbol_Mm.tab.gz' # NEW AVAILABLE?
+filename_mouse_to_human='ensembl_v82_Mm_Hs.tab.gz' # NEW AVAILABLE?
 
 
-# Load data
+# Step 5 - Load data
 sc_data=sc.read_10x_h5(filename_input_h5, genome)
 sc_data.var_names_make_unique()
 sc_data.obs['cell_labels'] = pd.read_csv(filename_cluster_ids, header=None, dtype='category')[1].values
 
 
-# Determine cell quality metrics (percentage mitochondrial genes, n.o. counts, n.o. genes)
+# Step 6 - Load mouse to human gene mapping
+mouse_mapping=pd.read_csv(filename_mouse_mapping, compression='gzip', index_col=1, sep='\t')
+mouse_mapping=mouse_mapping[[isinstance(x, str) for x in mouse_mapping.index]]
+mouse_to_human=pd.read_csv(filename_mouse_to_human, compression='gzip', index_col=0, sep='\t')
+
+
+# Step 7 - Determine cell quality metrics (percentage mitochondrial genes, n.o. counts, n.o. genes)
 mito_genes=sc_data.var_names.str.startswith('mt-')
 sc_data.obs['percent_mito'] = np.sum(sc_data[:, mito_genes].X, axis=1).A1 / np.sum(sc_data.X, axis=1).A1
 sc_data.obs['n_counts'] = sc_data.X.sum(axis=1).A1
-sc.pp.filter_cells(sc_data, min_counts=0)
+sc.pp.filter_cells(sc_data, min_genes=0)
 
 
-# Pre-process data
-sc.pp.normalize_per_cell(sc_data, counts_per_cell_after=1e4, key_n_counts='original_counts') # Normalize to 10,000 counts per cell
+# Step 8 - Plot pre-QC metrics
+sc.pl.violin(sc_data, ['n_genes', 'n_counts', 'percent_mito'], jitter=0.4, multi_panel=True, save='pre_qc_metrics.png')
+sc.pl.scatter(sc_data, x='n_counts', y='percent_mito', save='pre_qc_counts_mito.png')
+sc.pl.scatter(sc_data, x='n_counts', y='n_genes', save='pre_qc_counts_genes.png')
+
+
+# Step 9 - Run QC on the data
 sc.pp.filter_cells(sc_data, min_counts=1) # Retain cells with >1 UMI counts
 sc.pp.filter_cells(sc_data, min_genes=500) # Retain cells with >500 genes
 sc.pp.filter_cells(sc_data, max_genes=4000) # Retain cells with <4000 genes
 sc.pp.filter_genes(sc_data, min_cells=1) # Retain genes occuring in >1 cell
 sc.pp.filter_genes(sc_data, min_counts=1) # Retain genes with >1 UMI counts
-sc_data=sc_data[sc_data.obs['percent_mito'] < 0.1,:] # Retain cells with fewer than 10% mitochondrial genes
-sc.pp.log1p(sc_data) # Log transform the data
+sc_data.write_h5ad(filename_h5ad_out) # Save H5AD object for later use
+sc_data=sc_data[sc_data.obs['percent_mito'] < 0.1,:] # Retain cells with <10% mitochondrial genes
 
 
-# Restructure data
-sc_data_df=sc_data.to_df().T # Convert to Pandas DataFrame and transpose
+# Step 10 - Plot post-QC metrics
+sc.pl.violin(sc_data, ['n_genes', 'n_counts', 'percent_mito'], jitter=0.4, multi_panel=True, save='post_qc_metrics.png')
+sc.pl.scatter(sc_data, x='n_counts', y='percent_mito', save='post_qc_counts_mito.png')
+sc.pl.scatter(sc_data, x='n_counts', y='n_genes', save='post_qc_counts_genes.png')
+
+
+# Step 11 - Restructure data
+normalized_df=normalize(sc_data.to_df()) # Normalize gene expression across cells
 cell_cluster_ids=pd.read_csv(filename_cluster_ids, sep=',', header=None, index_col=0) # Load cluster ids for cells
 cell_cluster_ids.columns=['cluster_id'] # Set DataFrame column name
-sc_data_avg_by_celltype=get_average_by_celltype(sc_data_df, cell_cluster_ids) # Average gene expression across cell types
-sc_data_standardized_by_celltype=standardize(sc_data_avg_by_celltype) # Standardize gene expression across cell types
-sc_data_standardized_by_celltype.to_csv(filename_out, skip_blank_lines=True, sep='\t') # Export file
+averaged_per_cell_type_df=get_average_by_celltype(normalized_df.T, cell_cluster_ids) # Average gene expression across cell types
+mapped_to_human,not_mapped=to_ensembl(mouse_mapping, mouse_to_human, averaged_per_cell_type_df) # Map to human genes
+standardized_by_cell_type_df=standardize(mapped_to_human) # Standardize gene expression across cell types
+mapped_to_human.to_csv(filename_mapped_out, sep='\t') # Export mouse-to-human mapping results
+standardized_by_cell_type_df.to_csv(filename_restructured_out, sep='\t') # Export final restructured expression matrix
