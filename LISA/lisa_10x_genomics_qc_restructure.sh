b1e7c00c3cfa186bd652579158ac796f8ea30fc3
kjradem <koenrademaker@outlook.com>
2019-04-11 10:32:06 +0200

    fixed variable names, added output files

--- LISA/lisa_10x_genomics_qc_restructure.sh
+++ LISA/lisa_10x_genomics_qc_restructure.sh
@@ -23,17 +23,17 @@ out_dir="$TMPDIR"/10x_genomics_qc_pp/output # Create output directory
 mkdir ${out_dir}
 
 cp $HOME/Koen/SC_data/10x_Genomics/1M_neurons_filtered_gene_bc_matrices_h5.h5 ${work_dir} # Copy gene / cell matrix to work directory
-${h5_file}="$TMPDIR"/10x_genomics_qc_pp/1M_neurons_filtered_gene_bc_matrices_h5.h5
+h5_file="$TMPDIR"/10x_genomics_qc_pp/1M_neurons_filtered_gene_bc_matrices_h5.h5
 cp $HOME/Koen/SC_data/10x_Genomics/analysis/clustering/graphclust/clusters.csv ${work_dir} # Copy clustering output to work directory
-${cluster_file}="$TMPDIR"/10x_genomics_qc_pp/clusters.csv
+cluster_file="$TMPDIR"/10x_genomics_qc_pp/clusters.csv
 cp $HOME/Koen/SC_data/10x_Genomics/ensembl_v83_ensembl_mgisymbol_Mm.tab.gz ${work_dir} # Copy mouse mapping to work directory
-${mouse_mapping_file}="$TMPDIR"/10x_genomics_qc_pp/ensembl_v83_ensembl_mgisymbol_Mm.tab.gz
+mouse_mapping_file="$TMPDIR"/10x_genomics_qc_pp/ensembl_v83_ensembl_mgisymbol_Mm.tab.gz
 cp $HOME/Koen/SC_data/10x_Genomics/ensembl_v82_Mm_Hs.tab.gz ${work_dir} # Copy mouse-to-human mapping to work directory
-${mouse_human_mapping_file}="$TMPDIR"/10x_genomics_qc_pp/ensembl_v82_Mm_Hs.tab.gz
+mouse_human_mapping_file="$TMPDIR"/10x_genomics_qc_pp/ensembl_v82_Mm_Hs.tab.gz
 cd ${work_dir} # Move to work directory
 
 
-# (1) Execute Python program with the following arguments:
+# (1) Execute Python script with the following arguments:
 # - Gene / cell matrix - ${h5_file}
 # - Clustering output - ${cluster_file}
 # - Mouse mapping - ${mouse_mapping_file}
@@ -43,7 +43,8 @@ $HOME/Koen/Python-3.6.0/python ${script_path} ${h5_file} ${cluster_file} ${mouse
 
 
 # (2) File export
-mv 10x_1M_neurons_qc.h5ad ${out_dir} # Move to output directory
+mv lisa_10x_1M_qc_restructure.log ${out_dir} # Move log file to output directory
+mv 10x_1M_neurons_qc.h5ad ${out_dir} # Move processed H5AD object to output directory
 mv 10x_1M_neurons_qc_restructured.csv ${out_dir} # Move restructured data to output directory
 mv 10x_1M_neurons_qc_human_mapped.csv ${out_dir} # Move human-mapped data to output directory
 mv figures ${out_dir} # Move figures folder to output directory
