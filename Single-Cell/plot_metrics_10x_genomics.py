33ca6fb10af16e21607f1a09811fab8ee931accd
kjradem <koenrademaker@outlook.com>
2019-04-15 09:40:51 +0200

    added script to plot dataset metrics

---
+++ Single-Cell/plot_metrics_10x_genomics.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+
+
+# ABOUT: Script to plot metrics of 10x Genomics mouse brain scRNA-seq dataset
+# REQUIRED: - HFD5 gene / cell matrix (see README)
+# AUTHOR: Koen Rademaker
+# DATE: 12 April 2019
+
+
+# Step 1 - Import packages
+import sys
+import scanpy as sc
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+matplotlib.use('Agg')
+
+
+# Step 2 - Set Scanpy and Matplotlib settings
+sc.logging.print_versions()
+sc.settings.verbosity = 5
+sc.settings.logfile='plot_metrics_10x_genomics.log'
+sc.settings.autosave = True
+sc.settings.set_figure_params(dpi=300)
+rcParams['figure.figsize'] = 5, 5
+rcParams['savefig.dpi'] = 300
+matplotlib.style.use('classic')
+
+
+# Step 3 - Set variables
+filename_input_h5=sys.argv[1]
+reference_genome='mm10'
+
+
+# Step 4 - Import data
+sc_data = sc.read_10x_h5(filename_input_h5, reference_genome)
+sc_data.var_names_make_unique()
+
+
+# Step 5 - Calculate metrics
+mito_genes=sc_data.var_names.str.startswith('mt-')
+sc_data.obs['percent_mito'] = (np.sum(sc_data[:, mito_genes].X, axis=1).A1 / np.sum(sc_data.X, axis=1).A1)*100
+sc_data.obs['n_counts'] = sc_data.X.sum(axis=1).A1
+sc.pp.filter_cells(sc_data, min_genes=0)
+
+
+# Step 6 - Plot metrics
+# Step 6A - Plot relation between UMI counts and gene counts
+sc.pl.scatter(sc_data, x='n_counts', y='n_genes', save='_counts_genes.png')
+
+# Step 6B - Plot relation between UMI counts and % mtDNA
+sc.pl.scatter(sc_data, x='n_counts', y='percent_mito', save='_counts_mito.png')
+
+# Step 6C - Plot relation between gene counts and % mtDNA
+sc.pl.scatter(sc_data, x='n_genes', y='percent_mito', save='_genes_mito.png')
+
+# Step 6D - Plot distribution of UMI counts
+plt.hist(np.log10(sc_data.obs['n_counts']), bins=20, color='blue')
+mu=np.mean(np.log10(sc_data.obs['n_counts']))
+median=np.median(np.log10(sc_data.obs['n_counts']))
+sigma=np.std(np.log10(sc_data.obs['n_counts']))
+plt.xlabel('UMIS per cell (log10)')
+plt.ylabel('Frequency')
+plt.title('UMI Distribution')
+plt.axvline(x=mu-3*sigma, color='black', linestyle='--')
+plt.axvline(x=mu, color='r', linestyle='--')
+plt.axvline(x=median, color='g', linestyle='-')
+plt.axvline(x=mu+3*sigma, color='black', linestyle='--')
+plt.show()
+plt.savefig('UMI_distribution.png')
+plt.close()
+
+# Step 6E - Plot distribution of gene counts
+plt.hist(sc_data.obs['n_genes'], bins=40, color='blue')
+mu=np.mean(sc_data.obs['n_genes'])
+median=np.median(sc_data.obs['n_genes'])
+sigma=np.std(sc_data.obs['n_genes'])
+plt.xlabel('Genes per Cell')
+plt.ylabel('Frequency')
+plt.title('Gene Distribution')
+plt.axvline(x=mu-3*sigma, color='black', linestyle=':')
+plt.axvline(x=mu, color='r', linestyle='--')
+plt.axvline(x=median, color='g', linestyle='-')
+plt.axvline(x=mu+3*sigma, color='black', linestyle=':')
+plt.show()
+plt.savefig('Gene_distribution.png')
+plt.close()
+
+# Step 6F - Plot distribution of percentage of mitochondrial DNA (% mtDNA)
+plt.hist(sc_data.obs['percent_mito'], bins=100, range=(0, 10), color='blue')
+plt.xlabel('% mtDNA')
+plt.ylabel('Frequency')
+plt.title('% Mitochondrial DNA Distribution')
+plt.show()
+plt.savefig('mtDNA_distribution.png')
+plt.close()
