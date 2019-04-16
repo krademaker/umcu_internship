ee93a856af27181908cd808a85d95eb12cd9b855
kjradem <koenrademaker@outlook.com>
2019-04-16 14:02:25 +0200

    changed order, updated annotation, set width limit for % mtDNA

--- Single-Cell/Pipeline/get_qc_metrics_10x_genomics.py
+++ Single-Cell/Pipeline/get_qc_metrics_10x_genomics.py
@@ -39,8 +39,8 @@ print(filename_input_h5ad)
 sc_data=sc.read_h5ad(filename_input_h5ad)
 
 
-# Step 6 - Determine cell quality metrics (percentage mtDNA, n.o. UMIs, n.o. genes)
-# Percentage of mtDNA per cell
+# Step 6 - Determine cell quality metrics (% mtDNA, UMI count, gene count)
+# % mtDNA / cell
 mito_genes=sc_data.var_names.str.startswith('mt-')
 sc_data.obs['percent_mito']=np.sum(sc_data[:, mito_genes].X, axis=1).A1/np.sum(sc_data.X, axis=1).A1
 sc_data.obs['percent_mito'].to_csv(str(filename_input_h5ad)+'_percent_mito.csv')
@@ -48,14 +48,14 @@ mito_mean=np.mean(sc_data.obs['percent_mito'])
 mito_sd=np.std(sc_data.obs['percent_mito'])
 print('Mean %mtDNA / cell: ', mito_mean)
 print('SD %mtDNA / cell: ', mito_sd)
-# Number of UMIs per cell
+# UMI count / cell
 sc_data.obs['n_counts']=sc_data.X.sum(axis=1).A1
 sc_data.obs['n_counts'].to_csv(str(filename_input_h5ad)+'_n_counts.csv')
 umi_mean=np.mean(np.log10(sc_data.obs['n_counts']))
 umi_sd=np.std(np.log10(sc_data.obs['n_counts']))
 print('Mean UMIs / cell: ', umi_mean)
 print('SD UMIs / cell: ', umi_sd)
-# Number of genes per cell
+# Gene count / cell
 sc.pp.filter_cells(sc_data, min_genes=0)
 sc_data.obs['n_genes'].to_csv(str(filename_input_h5ad)+'_n_genes.csv')
 gene_mean=np.mean(sc_data.obs['n_genes'])
@@ -65,6 +65,18 @@ print('SD genes / cell: ', gene_sd)
 
 
 # Step 7 - Plot Matplotlib metrics
+# Distribution of % mtDNA
+plt.hist(sc_data.obs['percent_mito'],  bins=100, range=(0,25), color='blue')
+plt.xlabel('% mtDNA per cell')
+plt.ylabel('Frequency')
+plt.title('% Mitochondrial DNA Distribution [cut at 25%]')
+plt.axvline(x=mito_mean-3*mito_sd, color='black', linestyle='--', label='-3 σ')
+plt.axvline(x=mito_mean, color='red', linestyle='--', label='μ')
+plt.axvline(x=mito_mean+3*mito_sd, color='black', linestyle='--', label='+3 σ ('+str(np.round(mito_mean+3*mito_sd,decimals=1))+')')
+plt.legend(loc='upper right')
+plt.show()
+plt.savefig(str(sys.argv[1])+'_mtDNA_distribution.png')
+plt.close()
 # Distribution of UMI counts
 plt.hist(np.log10(sc_data.obs['n_counts']), bins=20, color='blue')
 plt.xlabel('UMIs per cell (log10)')
@@ -88,16 +100,4 @@ plt.axvline(x=gene_mean+3*gene_sd, color='black', linestyle='--', label='+3 σ')
 plt.legend(loc='upper right')
 plt.show()
 plt.savefig(str(sys.argv[1])+'_Gene_distribution.png')
-plt.close()
-# Distribution of % mtDNA
-plt.hist(sc_data.obs['percent_mito'],  bins=100, color='blue')
-plt.xlabel('% mtDNA per cell')
-plt.ylabel('Frequency')
-plt.title('% Mitochondrial DNA Distribution')
-plt.axvline(x=mito_mean-3*mito_sd, color='black', linestyle='--', label='-3 σ')
-plt.axvline(x=mito_mean, color='red', linestyle='--', label='μ')
-plt.axvline(x=mito_mean+3*mito_sd, color='black', linestyle='--', label='+3 σ ('+str(np.round(mito_mean+3*mito_sd,decimals=1))+')')
-plt.legend(loc='upper right')
-plt.show()
-plt.savefig(str(sys.argv[1])+'_mtDNA_distribution.png')
 plt.close()
