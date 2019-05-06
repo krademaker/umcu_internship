34af59a70660ab9f3ecac6c2eceb8b398c94c1f9
kjradem <koenrademaker@outlook.com>
2019-05-06 17:12:33 +0200

    include Python scripts for 2nd step of process

--- Single-Cell/KI-Data-DEPICT/ki_to_depict.sh
+++ Single-Cell/KI-Data-DEPICT/ki_to_depict.sh
@@ -3,10 +3,14 @@
 # GOAL:     Convert Karolinska Institue (KI) cluster-level mean gene expression data to DEPICT input
 # INPUT:    KI data as .rda files (see README)
 # AUTHOR:   Koen Rademaker
-# DATE:     1 May 2019
+# DATE:     5 May 2019
+
+script_path=~/Git/umcu_internship/Single-Cell/KI-Data-DEPICT
+data_path=~/Data/SingleCell_Data/Skene-2018
 
 ########## Run R script to generate cluster-level mean expression files ##########
-Rscript ki_to_cluster_mean_exp_matrix.R
+Rscript ${script_path}/ki_rda_to_matrix.R
 
 ########## Run Python script to restructure data for DEPICT analysis ##########
-    # TODO
+python ${script_path}/ki_matrix_to_depict.py ${data_path}/KI_mean_lvl1_exp.tsv ${data_path}/KI_lvl1_standardized.tsv
+python ${script_path}/ki_matrix_to_depict.py ${data_path}/KI_mean_lvl2_exp.tsv ${data_path}/KI_lvl2_standardized.tsv
