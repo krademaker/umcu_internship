7f829a6909ad0fd1baf92d16c02bcea50135b361
Koen Rademaker <24732704+kjradem@users.noreply.github.com>
2019-05-14 15:25:41 +0200

    corrected reference to R script, updated data

--- Single-Cell/10xGenomics/preprocess_cluster_composition.sh
+++ Single-Cell/10xGenomics/preprocess_cluster_composition.sh
@@ -3,7 +3,7 @@
 # GOAL: 	Compare the composition of clusters for different partitioned sizes of the 10x Genomics 1.3 million mouse brain cell dataset.
 # INPUT:	N clustering files, each consisting of one cell barcode column and one cell-clustering identity column.
 # AUTHOR:	Koen Rademaker
-# DATE:		13 May 2019
+# DATE:		14 May 2019
 
 ########## Set paths to scripts and files ##########
 visualization_script=~/umcu_internship/Single-Cell/10xGenomics/visualize_cluster_composition.R
