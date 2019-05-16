4c0503d6908143a0383d5e296ee23d5a1f19c03a
kjradem <koenrademaker@outlook.com>
2019-05-16 15:37:46 +0200

    code formatting

--- Single-Cell/10xGenomics/plot_cell_quality_metrics.py
+++ Single-Cell/10xGenomics/plot_cell_quality_metrics.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-# ABOUT: Script to plot pre- and post-QC cell quality metrics
-# REQUIRED: - H5AD object with pre-qc data (sc_data_pre_qc)
-#           - H5AD object with post_qc data (sc_data_post_qc)
-#           - String with sample 
+# ABOUT:        Script to plot pre- and post-QC cell quality metrics
+# PARAMETERS:   - H5AD object with pre-qc data (sc_data_pre_qc)
+#               - H5AD object with post_qc data (sc_data_post_qc)
+#               - Number of cells, used in plot titles (n_cells)
 # AUTHOR: Koen Rademaker
 # DATE: 16 May 2019
 
@@ -30,24 +30,24 @@ sc_data_post_qc = sc.read_h5ad(sys.argv[2])
 n_cells = sys.argv[3]
 
 ########## Calculate mean and standard deviation ##########
-mtdna_mean=np.mean(sc_data_pre_qc.obs['percent_mito'])*100
-mtdna_sd=np.std(sc_data_pre_qc.obs['percent_mito'])*100
+mtdna_mean = np.mean(sc_data_pre_qc.obs['percent_mito'])*100
+mtdna_sd = np.std(sc_data_pre_qc.obs['percent_mito'])*100
 
-umi_mean=np.mean(np.log10(sc_data_pre_qc.obs['n_counts']))
-umi_sd=np.std(np.log10(sc_data_pre_qc.obs['n_counts']))
+umi_mean = np.mean(np.log10(sc_data_pre_qc.obs['n_counts']))
+umi_sd = np.std(np.log10(sc_data_pre_qc.obs['n_counts']))
 
-gene_mean=np.mean(sc_data_pre_qc.obs['n_genes'])
-gene_sd=np.std(sc_data_pre_qc.obs['n_genes'])
+gene_mean = np.mean(sc_data_pre_qc.obs['n_genes'])
+gene_sd = np.std(sc_data_pre_qc.obs['n_genes'])
 
-cell_mean=np.mean(sc_data_pre_qc.var['n_cells'])
-cell_sd=np.std(sc_data_pre_qc.var['n_cells'])
+cell_mean = np.mean(sc_data_pre_qc.var['n_cells'])
+cell_sd = np.std(sc_data_pre_qc.var['n_cells'])
 
 
 ########## Generate pre-QC plots ##########
+# Calculate percentage of cell below and above the UMI cut-off values.
+perc_below_min_umi = np.round((len(np.log10(sc_data_pre_qc.obs['n_counts']).where(np.log10(sc_data_pre_qc.obs['n_counts']) < umi_mean-3*umi_sd).dropna())/len(np.log10(sc_data_pre_qc.obs['n_counts'])))*100, decimals=2)
+perc_above_max_umi = np.round((len(np.log10(sc_data_pre_qc.obs['n_counts']).where(np.log10(sc_data_pre_qc.obs['n_counts']) > umi_mean+3*umi_sd).dropna())/len(np.log10(sc_data_pre_qc.obs['n_counts'])))*100, decimals=2)
 # Plot annotated UMI distribution
-perc_below_min_umi=np.round((len(np.log10(sc_data_pre_qc.obs['n_counts']).where(np.log10(sc_data_pre_qc.obs['n_counts']) < umi_mean-3*umi_sd).dropna())/len(np.log10(sc_data_pre_qc.obs['n_counts'])))*100, decimals=2)
-perc_above_max_umi=np.round((len(np.log10(sc_data_pre_qc.obs['n_counts']).where(np.log10(sc_data_pre_qc.obs['n_counts']) > umi_mean+3*umi_sd).dropna())/len(np.log10(sc_data_pre_qc.obs['n_counts'])))*100, decimals=2)
-
 plt.hist(np.log10(sc_data_pre_qc.obs['n_counts']), bins=100, color='blue')
 plt.xlabel('UMIs per cell (log10)')
 plt.ylabel('Frequency')
@@ -56,16 +56,16 @@ plt.tight_layout()
 plt.axvline(x=umi_mean-3*umi_sd, color='red', linestyle='--', label='< µ-3σ (~'+str(perc_below_min_umi)+'% of cells)')
 plt.axvline(x=umi_mean, color='black', linestyle='--', label='µ')
 plt.axvline(x=umi_mean+3*umi_sd, color='red', linestyle=':', label='> µ+3σ (~'+str(perc_above_max_umi)+'% of cells)')
-plt.legend(loc='upper right', fontsize = 'small')
+plt.legend(loc='upper right', fontsize='small')
 plt.show()
 plt.savefig('PRE_QC_umi_distribution_annotated.png', dpi=600)
 plt.close()
 
-# Plot annotated gene distribution
+# Calculate percentage of cell below and above the gene cut-off values.
 perc_below_min_genes=np.round((len(sc_data_pre_qc.obs['n_genes'].where(sc_data_pre_qc.obs['n_genes'] < 1000).dropna())/len(sc_data_pre_qc.obs['n_genes']))*100, decimals=2)
 perc_above_max_genes=np.round((len(sc_data_pre_qc.obs['n_genes'].where(sc_data_pre_qc.obs['n_genes'] > gene_mean+3*gene_sd).dropna())/len(sc_data_pre_qc.obs['n_genes']))*100, decimals=2)
-
-plt.hist(sc_data_pre_qc.obs['n_genes'], bins = 100, color='blue')
+# Plot annotated gene distribution
+plt.hist(sc_data_pre_qc.obs['n_genes'], bins=100, color='blue')
 plt.xlabel('Genes per cell')
 plt.ylabel('Frequency')
 plt.title('Gene distribution for '+n_cells+' cells')
@@ -73,7 +73,7 @@ plt.tight_layout()
 plt.axvline(x=1000, color='red', linestyle='--', label='< 1000 (~'+str(perc_below_min_genes)+'% of cells)')
 plt.axvline(x=gene_mean, color='black', linestyle='--', label='µ')
 plt.axvline(x=gene_mean+3*gene_sd, color='red', linestyle=':', label='> µ+3σ (~'+str(perc_above_max_genes)+'% of cells)')
-plt.legend(loc='upper right', fontsize = 'small')
+plt.legend(loc='upper right', fontsize='small')
 plt.show()
 plt.savefig('PRE_QC_gene_distribution_annotated.png', dpi=600)
 plt.close()
@@ -89,7 +89,7 @@ plt.title('% mtDNA distribution for '+n_cells+' cells')
 plt.tight_layout()
 plt.axvline(x=mtdna_mean, color='black', linestyle='--', label='µ')
 plt.axvline(x=mtdna_mean+3*mtdna_sd, color='red', linestyle=':', label='> µ+3σ (~'+str(perc_exceeding_mito_threshold)+'% of cells)')
-plt.legend(loc='upper right', fontsize = 'small')
+plt.legend(loc='upper right', fontsize='small')
 plt.show()
 plt.savefig('PRE_QC_mtdna_distribution_annotated.png', dpi=600)
 plt.close()
@@ -107,7 +107,7 @@ plt.savefig('POST_QC_umi_distribution.png', dpi=600)
 plt.close()
 
 # Plot gene distribution
-plt.hist(sc_data_post_qc.obs['n_genes'], bins = 100, color='red')
+plt.hist(sc_data_post_qc.obs['n_genes'], bins=100, color='red')
 plt.xlabel('Genes per cell')
 plt.ylabel('Frequency')
 plt.title('Gene distribution for 108K cells after QC')
