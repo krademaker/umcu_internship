175ff7e716e39794a71c5ce5dea941971302fe90
kjradem <koenrademaker@outlook.com>
2019-06-18 13:16:07 +0200

    initialized script

---
+++ Single-Cell/plot_combined_gwas_magma_results.R
@@ -0,0 +1,10 @@
+# TITLE:    plot_combined_gwas_magma_results.R
+# ABOUT:    Script to plot MAGMA results and highlight significant cell types for different scRNA-seq datasets.
+# INPUT:    file_path_10x_genomics_linear: Combined GWAS MAGMA linear mode results for 10x Genomics dataset (16 cell types)
+# INPUT:    file_path_karolinska_linear: Combined GWAS MAGMA linear mode results for KI dataset level 1 (24 cell types)
+# INPUT:    file_path_macparland_linear: Combined GWAS MAGMA linear mode results for MacParland dataset (20 cell types)
+# INPUT:    file_path_10x_genomics_top10: Combined GWAS MAGMA top 10% mode results for 10x Genomics dataset (16 cell types)
+# INPUT:    file_path_karolinska_top10: Combined GWAS MAGMA top 10% mode results for KI dataset level 1 (24 cell types)
+# INPUT:    file_path_macparland_top10: Combined GWAS MAGMA top 10% mode results for MacParland dataset (20 cell types)
+# AUTHOR:   Koen Rademaker
+# DATE:     18 June 2019
