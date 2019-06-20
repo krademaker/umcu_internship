4b55a341b41805e5f9e7be5f65cc3b814a38145d
kjradem <koenrademaker@outlook.com>
2019-06-20 13:40:19 +0200

    updated code annotation, flow and plots

--- Single-Cell/10X-Genomics/10X_tsne_plot_cell_types.R
+++ Single-Cell/10X-Genomics/10X_tsne_plot_cell_types.R
@@ -1,9 +1,10 @@
 # TITLE:    10X_tsne_plot_cell_types.R
-# ABOUT:    Script to plot clustering/cell types for the full 10x Genomics dataset and for the 108K cells subset with QC applied
-# INPUT:    tsne_clusters_10x_genomics.xlsx: t-SNE coordinates and clustering results for the full dataset.
-# INPUT:    tsne_cell_types_10x_genomics_108k_subset_qc.xlsx: t-SNE coordinates and cell types identities for 108K subset with QC applied
+# ABOUT:    Script to plot clustering/cell types for the 10x Genomics dataset (full/108K cells/108K cell with QC applied)
+# INPUT:    tsne_clusters_10x_genomics_graphclust.tsv.gz: t-SNE coordinates and cluster identities for the full dataset
+# INPUT:    tsne_clusters_10x_genomics_kmeans.tsv.gz: t-SNE coordinates and cluster identities for the 108K subset
+# INPUT:    tsne_cell_types_10x_genomics_108k_qc_kmeans.tsv.gz: t-SNE coordinates and cell type identities for the 108K subset with QC applied
 # AUTHOR:   Koen Rademaker
-# DATE:     13 June 2019
+# DATE:     20 June 2019
 
 ########## Load required libraries ##########
 library(ggplot2)
@@ -11,12 +12,12 @@ library(dplyr)
 library(ggrepel)
 
 ########## Load input data ##########
-graphclust_all_cells_df <- read.delim('~/Git/umcu_internship/Single-Cell/data/tsne_clusters_10x_genomics_graphclust.tsv.gz')
-kmeans_all_cells_df <- read.delim('~/Git/umcu_internship/Single-Cell/data/tsne_clusters_10x_genomics_kmeans.tsv.gz')
-kmeans_qc_cells_df <- read.delim("~/Git/umcu_internship/Single-Cell/data/tsne_cell_types_10x_genomics_108k_qc_kmeans.tsv.gz")
+graphclust_all_cells_df <- read.delim('~/umcu_internship/Single-Cell/data/tsne_clusters_10x_genomics_graphclust.tsv.gz')
+kmeans_all_cells_df <- read.delim('~/umcu_internship/Single-Cell/data/tsne_clusters_10x_genomics_kmeans.tsv.gz')
+kmeans_qc_cells_df <- read.delim("~/umcu_internship/Single-Cell/data/tsne_cell_types_10x_genomics_108k_qc_kmeans.tsv.gz")
 
 ########## Format data for plotting ##########
-# Graph-based clustering
+# (1) Graph-based clustering
 graphclust_all_cells_df$Cluster <- factor(graphclust_all_cells_df$Cluster)
 graphclust_all_cells_labels <- data.frame(Cluster = levels(graphclust_all_cells_df$Cluster),
                                           label = paste0('Cluster ', levels(graphclust_all_cells_df$Cluster)))
@@ -25,7 +26,7 @@ graphclust_all_cells_labels_2 <- graphclust_all_cells_df %>%
     summarize(TSNE.1 = mean(TSNE.1), TSNE.2 = mean(TSNE.2)) %>%
     left_join(graphclust_all_cells_labels)
 
-# K-means clustering
+# (2) K-means clustering
 kmeans_all_cells_df$Cluster <- factor(kmeans_all_cells_df$Cluster)
 kmeans_all_cells_labels <- data.frame(Cluster = levels(kmeans_all_cells_df$Cluster),
                                       label = paste0('Cluster ', levels(kmeans_all_cells_df$Cluster)))
@@ -34,7 +35,7 @@ kmeans_all_cells_labels_2 <- kmeans_all_cells_df %>%
     summarize(TSNE.1 = mean(TSNE.1), TSNE.2 = mean(TSNE.2)) %>%
     left_join(kmeans_all_cells_labels)
 
-# K-means clustering, assigned cell types
+# (3) K-means clustering, assigned cell types
 kmeans_qc_cells_df$Cluster <- factor(kmeans_qc_cells_df$Cluster)
 kmeans_qc_cells_labels <- data.frame(Cluster = levels(kmeans_qc_cells_df$Cluster), 
                                      label = levels(kmeans_qc_cells_df$Cluster))
@@ -74,4 +75,4 @@ ggplot(kmeans_qc_cells_df, aes(x = TSNE.1, y = TSNE.2, color = Cluster)) +
     ylab('t-SNE dimension 2') +
     theme(plot.title = element_text(hjust = 0.5)) +
     theme(legend.position = 'none') +
-    ggrepel::geom_label_repel(data = kmeans_qc_cells_labels_2, aes(label = label), size=3)
+    ggrepel::geom_label_repel(data = kmeans_qc_cells_labels_2, aes(label = label), size=2.75)
