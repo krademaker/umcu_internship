469f095cb42d76679f37e07fb4dbfda964054d11
kjradem <koenrademaker@outlook.com>
2019-07-01 17:13:07 +0200

    updated to convert mouse > human genes, partition by chromosome and create gene set files

--- Single-Cell/LDSC/prepare_annotation_data.R
+++ Single-Cell/LDSC/prepare_annotation_data.R
@@ -12,47 +12,104 @@ library(dplyr)
 library(tibble)
 
 ########## Declare custom functions ##########
-get_specificity_quantile_genes <- function(ctd, cell_type, quantile){
+get_specificity_decile_genes <- function(ctd, cell_type, decile){
     genes <- as.data.frame(ctd[[1]]$specificity_quantiles[, cell_type]) %>%
         rownames_to_column('gene') %>%
-        filter(ctd_10X[[1]]$specificity_quantiles[, ct] == quantile) %>%
+        filter(ctd_10X[[1]]$specificity_quantiles[, cell_type] == decile) %>%
         pull(gene)
     return(genes)
 }
 
+get_homolog_translation <- function(){
+    # Load orthology data
+    all_homologs = load.homologs()
+    ortholog_data = analyse.orthology('mouse', 'human', all_homologs)$orthologs_one2one
+    # Select human and mouse entries
+    selected_homologs <- all_homologs %>%
+        filter(all_homologs$'HomoloGene ID' %in% ortholog_data$'HomoloGene ID' & all_homologs$'Common Organism Name' %in% c('mouse, laboratory', 'human')) %>%
+        select('HomoloGene ID', 'Common Organism Name', 'Symbol')
+    # Join mouse-human homologs together
+    selected_homologs <- full_join(selected_homologs %>% filter(selected_homologs$`Common Organism Name` == 'mouse, laboratory'),
+                                   selected_homologs %>% filter(selected_homologs$`Common Organism Name` == 'human'),
+                                   by = 'HomoloGene ID')
+    # Rename columns and set mouse gene symbols as row names
+    selected_homologs <- selected_homologs %>%
+        select('HomoloGene ID', 'Symbol.x', 'Symbol.y') %>%
+        rename('Symbol_Mouse' = 'Symbol.x', 'Symbol_Human' = 'Symbol.y')
+    selected_homologs <- selected_homologs %>% distinct()
+    row.names(selected_homologs) <- selected_homologs$'Symbol_Mouse'
+    selected_homologs <- selected_homologs %>%
+        select('HomoloGene ID', 'Symbol_Human')
+    return(selected_homologs)
+}
+
+translate_mouse_symbol_to_ensembl <- function(genes_mouse_symbol){
+    # Convert mouse gene symbols to human gene symbols
+    genes_mouse_symbol_df <- as.data.frame(genes_mouse_symbol)
+    genes_mouse_symbol_df[] <- setNames(homolog_translation$Symbol_Human, row.names(homolog_translation))[as.character(unlist(genes_mouse_symbol_df))]
+    genes_human_symbol <- as.vector(unlist(genes_mouse_symbol_df))
+    # Convert human gene symbols to human gene Ensembl IDs
+    genes_human_symbol_df <- as.data.frame(genes_human_symbol)
+    genes_human_symbol_df[] <- setNames(human_gene_reference$GENE_ID, human_gene_reference$GENE_NAME)[as.character(unlist(genes_human_symbol_df))]
+    genes_human_ensembl <- as.vector(unlist(genes_human_symbol_df))
+    return(genes_human_ensembl)
+}
+
+partition_genes_by_chromosome <- function(genes, cell_type, decile, out_dir='~/Data/LDSC/'){
+    for (chr in 1:22){
+        # Partition genes per chromosome
+        chr_partitioned_genes <- human_gene_reference %>%
+            filter(human_gene_reference$CHROMOSOME == chr & human_gene_reference$GENE_ID %in% genes) %>%
+            select('GENE_ID')
+        # Write file
+        f <- file(paste(out_dir, '10x_Genomics.', cell_type, '.', decile, '.', chr, '.GeneSet', sep = ''), open = 'w')
+        write.table(chr_partitioned_genes, file = f, sep = '\t', row.names = F, col.names = F, quote = F)
+        close(f)
+    }
+}
 
 ########## Set variables ##########
 mean_expr_path <- '~/Git/umcu_internship/Single-Cell/data/10x_Genomics_16_cell_types_mean_expression.tsv'
+human_gene_reference_path <- '~/Git/umcu_internship/Single-Cell/data/gencode_v19_genes_only.tab'
 formal_cell_type_names <- c('Glutamatergic neurons', 'Neuroblasts 1', 'Astrocytes 1', 'Neuroblasts 2', 'Intermediate progenitors', 'Enteric glial cells', 'Interneurons 1', 'Neurons', 'Interneurons 2', 'Vascular endothelial cells', 'Enteric neurons', 'Cajal-Retzius cells', 'Oligodendrocytes', 'Astrocytes 2', 'Endothelial cells', 'Microglia')
+
+########## Load human gene reference data and remove Ensembl ID version numbers ##########
+human_gene_reference <- read.delim(human_gene_reference_path)
+human_gene_reference$GENE_ID <- sapply(strsplit(as.character(human_gene_reference$GENE_ID), '\\.'), `[`, 1)
 
 ########## Load cell type-level mean expression data & calculate specificity metric S(g,c) ##########
 mean_expr_data <- read.delim(mean_expr_path, row.names = 1)
 specificity_data <- mean_expr_data/rowSums(mean_expr_data)
+
+########## Load homolog translation table ##########
+homolog_translation <- get_homolog_translation()
 
 ########## Restructure ctd object for MAGMA cell type association analysis ##########
 ctd_10X <- MAGMA.Celltyping::ctd_allKI
 ctd_10X[[1]]$mean_exp <- mean_expr_data
 ctd_10X[[1]]$specificity <- specificity_data
 
-########## Calculate specificity quantiles in 41 bins (least-to-most specific expression) ##########
+########## Calculate specificity deciles (least-to-most specific expression) ##########
 ctd_10X = prepare.quantile.groups(ctd_10X, specificity_species = 'mouse', numberOfBins = 10)
 ctd_10X[[2]] <- NULL
 
 ########## Store unmapped genes ##########
 unmapped_genes <- row.names(specificity_data)[!(row.names(specificity_data) %in% row.names(ctd_10X[[1]]$specificity_quantiles))]
 
-########## Get genes for all categories ##########
-for (ct in names(ctd_10X[[1]]$specificity)){
-    anno_non_expr <- get_specificity_quantile_genes(ctd_10X, ct, 0)
-        # get human chromosome number
-        # split genes per chromosome
-        # convert from gene symbol to ENSEMBL gene ID
-        # save to file as 10x_Genomics.[celltype].N.[chr].Geneset
-            
-    
-    # range 1-10
-        # get human chromosome number
-        # split genes per chromosome
-        # convert from gene symbol to ENSEMBL gene ID
-        # save to file as 10x_Genomics.[celltype].[n].[chr].Geneset
+########## Create gene set files ##########
+for (cell_type in names(ctd_10X[[1]]$specificity)){
+    # Genes not expressed in a cell type
+    cell_type_non_expressed <- get_specificity_decile_genes(ctd_10X, cell_type, 0)
+    ensembl_cell_type_non_expressed <- translate_mouse_symbol_to_ensembl(cell_type_non_expressed)
+    partition_genes_by_chromosome(genes = ensembl_cell_type_non_expressed,
+                                  cell_type = cell_type,
+                                  decile = 'N')
+    # Genes in specificity decile of cell type 
+    for (decile_n in 1:10){
+        cell_type_decile_expressed <- get_specificity_decile_genes(ctd_10X, cell_type, decile_n)
+        ensembl_cell_type_decile_expressed <- translate_mouse_symbol_to_ensembl(cell_type_decile_expressed)
+        partition_genes_by_chromosome(genes = ensembl_cell_type_non_expressed,
+                                      cell_type = cell_type,
+                                      decile = toString(decile_n))
+    }
 }
