1da65d46b338e8e5348d15fd84510236e653b544
kjradem <koenrademaker@outlook.com>
2019-07-03 13:22:09 +0200

    added initialization of script

--- Single-Cell/LDSC/calculate_ld_scores.sh
+++ Single-Cell/LDSC/calculate_ld_scores.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# TITLE:
-# GOAL:
-# INPUT:
-# INPUT:
-# OUTPUT:
-# AUTHOR:
-# DATE:
+# TITLE:	calculate_ld_scores.sh
+# GOAL:		Calculate LD scores for specificity deciles of cell types
+# INPUT:	LDSC thin-annotation files (annot.gz) per autosomal chromosome for all cell types
+# INPUT:	1000 Genomes Project phase 3 plink files, HapMap3 SNPs (will automatically be downloaded and organized)
+# OUTPUT:	LD score output files; .M (total number of SNPs), .M_5_50 (number of SNPs with MAF > 5%), .ldscore.gz (SNP LD scores)
+# AUTHOR:	Koen Rademaker
+# DATE:		3 July 2019
 
 
 ########## Initialize script ##########
 # Set folder paths
+ldsc_dir=~/umcu_internship/Single-Cell/LDSC/ldsc
+files_dir=~/umcu_internship/Single-Cell/LDSC/Files
+annotation_dir=${files_dir}/Annotation
+ld_scores_dir=${files_dir}/LD-Scores
+tmp_dir=${files_dir}/tmp
 # Create folder paths
+mkdir -p ${tmp_dir}
+mkdir -p ${ld_scores_dir}
 # Declare variables
+declare -a cell_types=("Glutamatergic_neurons" "Neuroblasts_1" "Astrocytes_1" "Neuroblasts_2" "Intermediate_progenitors" "Enteric_glial_cells" "Interneurons_1" "Neurons" "Interneurons_2" "Vascular_endothelial_cells" "Enteric_neurons" "Cajal_Retzius_cells" "Oligodendrocytes" "Astrocytes_2" "Endothelial_cells" "Microglia")
+declare -a specificity_deciles=(N 1 2 3 4 5 6 7 8 9 10)
 
 
 ########## Organize LDSC ##########
