297fb48d8b78a0dcf92afcf711086e649b3870e2
kjradem <koenrademaker@outlook.com>
2019-07-03 13:15:49 +0200

    initialized script to automatically calculate LD scores

---
+++ Single-Cell/LDSC/calculate_ld_scores.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# TITLE:
+# GOAL:
+# INPUT:
+# INPUT:
+# OUTPUT:
+# AUTHOR:
+# DATE:
+
+
+########## Initialize script ##########
+# Set folder paths
+# Create folder paths
+# Declare variables
+
+
+########## Organize LDSC ##########
+# Download background data
+	# PLINK
+	# HapMap3 SNPS
+# Declare variables
+# Activate LDSC environment
+
+
+########## Calculate LD scores with annotation files ##########
+# (1) Iterate over cell types
+# (2) Iterate over chromosomes
+# (3) Copy annotation file to temporary folder
+# (4) Calculate LD scores
+# (5) Merge files together for export
+# (6) Export
+# (7) Clear temporary folder
