a7176658a66c93a1c4f5ff2e5fc36e41e8bfb431
kjradem <koenrademaker@outlook.com>
2019-07-11 14:40:00 +0200

    tweaked plots

--- Single-Cell/plot_SCZ_combined_enrichment_results.R
+++ Single-Cell/plot_SCZ_combined_enrichment_results.R
@@ -13,9 +13,13 @@ library(cowplot)
 ########## Set variables ##########
 file_path_10x <- '~/umcu_internship/Single-Cell/data/10x_Genomics_SCZ_enrichment_results.csv'
 file_path_ki <- '~/umcu_internship/Single-Cell/data/KI_SCZ_enrichment_results.csv'
-cell_type_order_10x <- c('Neuroblasts (2)', 'Neurons (glutamatergic)', 'Interneurons (1)', 'Neuroblasts (1)', 'Interneurons (2)', 'Neurons', 'Cajal-Retzius cells', 'Oligodendrocytes', 'Vascular endothelial cells', 'Intermediate progenitors', 'Astrocytes (2)', 'Enteric glial cells', 'Enteric neurons', 'Astrocytes (1)', 'Endothelial cells', 'Microglia')
+cell_type_order_10x <- c('Neurons (glutamatergic)', 'Neuroblasts (2)', 'Neurons', 'Interneurons (1)', 'Oligodendrocytes', 'Interneurons (2)', 'Neuroblasts (1)', 'Intermediate progenitors', 'Cajal-Retzius cells', 'Vascular endothelial cells', 'Astrocytes (2)', 'Enteric glial cells', 'Enteric neurons', 'Astrocytes (1)', 'Endothelial cells', 'Microglia')
 cell_type_order_ki <- c('Pyramidal (CA1)', 'Pyramidal (SS)', 'Medium spiny neuron', 'Interneurons', 'Striatal interneuron', 'Dopaminergic neuron', 'Hypoth. GABAergic neuron', 'Hypoth. DA neurons', 'Oligodendrocytes', 'Serotonergic neuron', 'Oligodendrocyte precursors', 'Embr. GABA neuron', 'DA neuroblast', 'Embr. DA neuron', 'Neuroblasts', 'Oxytocin/vasopressin neurons', 'Astrocytes / Ependymal', 'Embr. midbrain nucl. neuron', 'Radial glia like', 'Hypoth. glutamat. neuron', 'Neural progenitors', 'Vasc. leptomeningeal cells', 'Microglia', 'Endothelial-Mural')
 n_gwas <- 6
+threshold_10x <- -log10(0.05/(16*n_gwas))
+ki_threshold <- -log10(0.05/(24*n_gwas))
+ki_strict_threshold <- -log10(0.05/((24+149)*8))
+
 
 
 ########## Load schizophrenia enrichment results ##########
@@ -35,8 +39,9 @@ ggplot(scz_enrichment_10x,
            y = neg_log10_p,
            fill = Method)) +
     geom_bar(stat = 'identity', position = 'dodge') +
-    geom_hline(yintercept = -log10(0.05/(as.numeric(length(unique(scz_enrichment_10x$Cell.type))*n_gwas))), colour = 'black') +
-    theme(legend.position = c(0.7, 0.95), legend.title = element_blank(), legend.text = element_text(size = 12)) +
+    geom_hline(yintercept = threshold_10x, colour = 'black') +
+    theme_classic(14) +
+    theme(legend.position = c(0.86, 0.9), legend.title = element_blank(), legend.text = element_text(size = 11)) +
     coord_flip() +
     ylab(expression('-log'[10] *' (P value)')) +
     xlab('')
@@ -46,8 +51,10 @@ ggplot(scz_enrichment_ki,
            y = neg_log10_p,
            fill = Method)) +
     geom_bar(stat = 'identity', position = 'dodge') +
-    geom_hline(yintercept = -log10(0.05/(as.numeric(length(unique(scz_enrichment_ki$Cell.type))*n_gwas))), colour = 'black') +
-    theme(legend.position = c(0.7, 0.95), legend.title = element_blank(), legend.text = element_text(size = 12)) +
+    geom_hline(yintercept = ki_threshold, colour = 'black') +
+    geom_hline(yintercept = ki_strict_threshold, colour = 'red') +
+    theme_classic(12) +
+    theme(legend.position = c(0.9, 0.9), legend.title = element_blank(), legend.text = element_text(size = 11)) +
     coord_flip() +
     ylab(expression('-log'[10] *' (P value)')) +
     xlab('')
