ec834fd2c42514b6d886c8df28d1624eedbc55db
kjradem <koenrademaker@outlook.com>
2019-07-31 11:13:07 +0200

    updated hapmap3 SNPs file, minor tweaks to variables

--- Single-Cell/LDSC/calculate_ld_scores.sh
+++ Single-Cell/LDSC/calculate_ld_scores.sh
@@ -6,33 +6,38 @@
 # INPUT:	1000 Genomes Project phase 3 plink files, HapMap3 SNPs (will automatically be downloaded and organized)
 # OUTPUT:	LD score output files; .M (total number of SNPs), .M_5_50 (number of SNPs with MAF > 5%), .ldscore.gz (SNP LD scores)
 # AUTHOR:	Koen Rademaker
-# DATE:		11 July 2019
+# DATE:		31 July 2019
 
 
 ########## Initialize script ##########
 # Set folder paths
-ldsc_dir=~/Git/umcu_internship/Single-Cell/LDSC/ldsc
-files_dir=~/Git/umcu_internship/Single-Cell/LDSC/Files
+ldsc_dir=~/umcu_internship/Single-Cell/LDSC/ldsc
+files_dir=~/umcu_internship/Single-Cell/LDSC/Files
 annotation_dir=${files_dir}/Annotation
 ld_scores_dir=${files_dir}/LD-Scores
 tmp_dir=${files_dir}/tmp
+miniconda_dir=~/miniconda2
 # Create folder paths
 mkdir -p ${tmp_dir}
-mkdir -p ${ld_scores_dir}
 # Declare variables
 declare -a cell_types=("Microglia" "Endothelial_cells" "Astrocytes_2" "Oligodendrocytes" "Cajal_Retzius_cells" "Enteric_neurons" "Vascular_endothelial_cells" "Interneurons_2" "Neurons" "Interneurons_1" "Enteric_glial_cells" "Intermediate_progenitors" "Neuroblasts_2" "Astrocytes_1" "Neuroblasts_1" "Glutamatergic_neurons")
 
 
 ########## Organize LDSC ##########
 # Download background data
+echo "-- Downloading 1000 Genomes Project phase 3 plink files--"
+wget https://data.broadinstitute.org/alkesgroup/LDSCORE/1000G_Phase3_plinkfiles.tgz -P ${files_dir}
+tar -xvzf ${files_dir}/1000G_Phase3_plinkfiles.tgz
 echo "-- Downloading HapMap3 SNPs --"
-wget https://data.broadinstitute.org/alkesgroup/LDSCORE/hapmap3_snps.tgz -P ${files_dir}
-tar -xvzf ${files_dir}/hapmap3_snps.tgz
+wget https://data.broadinstitute.org/alkesgroup/LDSCORE/w_hm3.snplist.bz2 -P ${files_dir}
+bzip2 -d ${files_dir}/w_hm3.snplist.bz2
+tail -n+2 ${files_dir}/w_hm3.snplist | cut -f1 > ${files_dir}/w_hm3.snplist.snps
 # Declare variables
 phase3_1000G_dir=${files_dir}/1000G_EUR_Phase3_plink
-hapmap3_dir=${files_dir}/hapmap3_snps
 ld_window=1
 dataset="10x_Genomics"
+# Activate LDSC environment
+source ${miniconda_dir}/bin/activate ldsc
 
 
 ########## Calculate LD scores with annotation files ##########
@@ -47,7 +52,7 @@ for ct in ${cell_types[@]}; do
 			--ld-wind-cm ${ld_window} \
 			--annot ${annotation_dir}/${dataset}_${ct}.${chr}.annot.gz \
 			--thin-annot \
-			--out ${ld_scores_dir}/${dataset}_${ct}_${chr} \
-			--print-snps ${hapmap3_dir}/hm.${chr}.snp
+			--print-snps ${files_dir}/w_hm3.snplist.snps \
+			--out ${ld_scores_dir}/${dataset}_${ct}_${chr}
 	done
 done
